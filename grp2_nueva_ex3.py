e70349bede40b14ed4d7b1a727073576dbf1f98b
rpnueva@paterostechnologicalcollege.edu.ph <rpnueva@paterostechnologicalcollege.edu.ph>
2026-03-11 11:25:25 +1000

    Create grp2_nueva_ex3.py with basic Python examples
    Added a simple Python script demonstrating variables and printing.

---
+++ grp2_nueva_ex3.py
@@ -0,0 +1,29 @@
+# Python Comments
+# My first simple script
+print("Hello, I'm Ruzzel Xyrus Palmar Nueva! from BSIT-1V")
+
+# Python Variables
+age = 19
+name = "Ruzzel"
+print(age)
+print(name)
+
+# Python - Variable Names 
+adrian = "Member 1, Adrian"
+angel = "Member 2, Angel"
+lyka = "Member 3, Lyka"
+marc = "Member 4, Marc"
+print(adrian)
+print(angel)
+print(lyka)
+print(marc)
+
+# Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+a = b = c = "Pateros Technological College"
+print(a)
